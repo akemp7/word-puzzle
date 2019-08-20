da635312c7cccdb4dc1d91c56d34736780cf869b
Anita Kemp and Jacqueline Remmel <anita@>
2019-08-20 09:18:52 -0700

    Add JS for building puzzle sentence.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,19 @@ $(document).ready(function() {
   event.preventDefault();
 
   var sentenceInput = $("#sentence").val();
-  
+  var buildPuzzle = "";
+  for (var i = 0; i < sentenceInput.length; i++) {
+    var letter = "";
+    if (sentenceInput[i] === "a") {
+      letter = "-";
+    } else {
+      letter = sentenceInput[i];
+    }
+    buildPuzzle += letter;
+    console.log(buildPuzzle);
+  }
+
+  $("#output").text(buildPuzzle);
 
   });
   $("#clear").click(function() {
